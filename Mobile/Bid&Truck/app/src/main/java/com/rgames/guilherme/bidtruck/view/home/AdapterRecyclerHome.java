d4f6551a88ec19f92b19584a476e4e36fe906599
willigro <willi.gro@hotmail.com>
2017-08-28 09:57:54 -0300

    Implementada a base da home

---
+++ Mobile/Bid&Truck/app/src/main/java/com/rgames/guilherme/bidtruck/view/home/AdapterRecyclerHome.java
@@ -0,0 +1,41 @@
+package com.rgames.guilherme.bidtruck.view.home;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rgames.guilherme.bidtruck.R;
+
+import java.util.List;
+
+public class AdapterRecyclerHome extends RecyclerView.Adapter<AdapterRecyclerHome.MyViewHolder> {
+
+    private List<Object> mList;
+
+    public AdapterRecyclerHome(List<Object> list) {
+        mList = list;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_recycler_home, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- Mobile/Bid&Truck/app/src/main/java/com/rgames/guilherme/bidtruck/view/home/HomeFragment.java
+++ Mobile/Bid&Truck/app/src/main/java/com/rgames/guilherme/bidtruck/view/home/HomeFragment.java
@@ -1,16 +1,23 @@
 package com.rgames.guilherme.bidtruck.view.home;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.rgames.guilherme.bidtruck.R;
+import com.rgames.guilherme.bidtruck.model.basic.MyProgressBar;
 
 public class HomeFragment extends Fragment {
+
     private View mView;
+    private MyProgressBar myProgressBar;
 
     public HomeFragment() {
     }
@@ -21,9 +28,74 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        initList();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return mView = inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            finishProgressBar();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected void onPreExecute() {
+                try {
+                    initProgressBar();
+                } catch (Exception e) {
+                    //sem tratamento, so na maciota por enquanto.
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                try {
+                    initRecyclerView();
+                    finishProgressBar();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute();
+    }
+
+    private void initRecyclerView() throws Exception {
+        RecyclerView r = mView.findViewById(R.id.recyclerview);
+        if (getActivity() != null)
+            r.setLayoutManager(new LinearLayoutManager(getActivity()));
+        else
+            throw new NullPointerException("Context nulo");
+        r.setAdapter(new AdapterRecyclerHome(null));
+    }
+
+    private void initProgressBar() throws ClassCastException, NullPointerException {
+        if (myProgressBar == null)
+            myProgressBar = new MyProgressBar((FrameLayout) mView.findViewById(R.id.frame_progress));
+    }
+
+    private void finishProgressBar() throws Exception {
+        if (myProgressBar != null) {
+            myProgressBar.onFinish();
+        }
     }
 }
 
