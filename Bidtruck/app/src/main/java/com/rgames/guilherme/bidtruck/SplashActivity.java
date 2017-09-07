95cc30b37bb4243a6b0bab688e7f9f6936381a19
kevyn <kevynh49@gmail.com>
2017-09-07 12:07:52 -0300

    Telas

--- Bidtruck/app/src/main/java/com/rgames/guilherme/bidtruck/SplashActivity.java
+++ Bidtruck/app/src/main/java/com/rgames/guilherme/bidtruck/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AwesomeSplash {
 
         //Customize Circular Reveal
         configSplash.setBackgroundColor(R.color.gray_very_light); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_BOTTOM);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or Flags.REVEAL_TOP
 
@@ -28,28 +28,28 @@ public class SplashActivity extends AwesomeSplash {
 
         //Customize Logo
         configSplash.setLogoSplash(R.drawable.bridge);
-        configSplash.setAnimLogoSplashDuration(2000);
+        configSplash.setAnimLogoSplashDuration(1000);
         configSplash.setAnimLogoSplashTechnique(Techniques.DropOut);
 
 
-        //Customize Path
-      //  configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
+      /*  Customize Path
+        configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
         configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
         configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
         configSplash.setAnimPathStrokeDrawingDuration(2000);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
         configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
         configSplash.setAnimPathFillingDuration(2000);
-        configSplash.setPathSplashFillColor(R.color.colorPrimaryLight); //path object filling color
+        configSplash.setPathSplashFillColor(R.color.colorPrimaryLight); //path object filling color*/
 
 
         //Customize Title
         configSplash.setTitleSplash("Bid & Track");
         configSplash.setTitleTextColor(R.color.colorAccent);
         configSplash.setTitleTextSize(50f); //float value
-        configSplash.setAnimTitleDuration(2000);
+        configSplash.setAnimTitleDuration(1000);
         configSplash.setAnimTitleTechnique(Techniques.FlipInX);
-        //configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+        configSplash.setTitleFont("fonts/museo500.otf"); //provide string to your font located in assets/fonts/
 
     }
 
@@ -57,6 +57,7 @@ public class SplashActivity extends AwesomeSplash {
     public void animationsFinished() {
         Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
